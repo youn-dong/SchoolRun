14ef574083bd5951798579908437f2f2e1d80e1d
윤동영 <ehddud9608@naver.com>
2025-03-09 18:45:28 +0900

    [Feat] 장애물 및 Ground 배치
    장애물 및 Ground 배치

---
+++ Assets/02_Scripts/GameObject/Board.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Board : MonoBehaviour
+{
+    private GameObject panel;
+
+}
